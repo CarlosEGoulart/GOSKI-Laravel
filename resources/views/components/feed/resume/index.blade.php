7937e608f48a53e2f99a7357021af3f92ec4aa86
goulartoliveiracarloseduardo <goulartoliveiracarloseduardo@gmail.com>
2026-03-04 15:38:19 +0000

    Resume-Posts Grid

--- resources/views/components/feed/resume/index.blade.php
+++ resources/views/components/feed/resume/index.blade.php
@@ -2,25 +2,26 @@
     <div class='flex items-center justify-start'>
         <img src='{{ $user->profile_photo_url ?? asset('images/icons/icon.png') }}' alt='Profile Picture'
             class='h-10 w-10 rounded-full object-cover'>
-        <h1 class='p-4 text-center text-2xl'>{{ Auth::user()->username }}</h1>
+        <h1 class='p-4 text-center text-2xl text-shadow-2xs'>{{ Auth::user()->username }}</h1>
     </div>
     <button
-        class="inset-ring-1 inset-ring-white/5 w-l inline-flex cursor-pointer justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white hover:bg-black/30">Meu
-        perfil</button>
+        class="inset-ring-1 inset-ring-white/5 w-l inline-flex cursor-pointer justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white hover:bg-black/30 shadow-xl">
+        Meu Perfil
+    </button>
 </div>
 
 <div class='flex flex-row justify-between py-4'>
     <div>
-        <h1>Seguidores</h1>
+        <h1 class='text-shadow-2xs'>Seguidores</h1>
     </div>
     <div>
-        <h1>Seguindo</h1>
+        <h1 class='text-shadow-2xs'>Seguindo</h1>
     </div>
 </div>
 
-<div class='flex flex-row justify-center py-4'>
-    <div>
-        <h1>Publicações</h1>
+<div class='flex flex-row justify-center'>
+    <div class='flex flex-col items-center justify-center gap-4'>
+        <h1 class='text-shadow-2xs'>Publicações</h1>
         <x-feed.resume.posts-resume />
     </div>
 </div>
--- resources/views/components/feed/resume/posts-resume.blade.php
+++ resources/views/components/feed/resume/posts-resume.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <h1>POST</h1>
+<div class="flex items-center justify-center">
+    <div class='grid grid-cols-3 grid-rows-3 gap-4'>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+        <div class='flex justify-center items-center bg-[#B9B9B9] h-35 w-35 rounded-md shadow-xl'>Post</div>
+    </div>
+
 </div>
